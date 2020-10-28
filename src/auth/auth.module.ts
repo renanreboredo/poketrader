10c749716ea68ec88d1453f183694162c7440df1
Renan Reboredo <renanrreboredo@gmail.com>
2020-10-28 03:23:13 -0300

    Implement local and jwt strategies for authentication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
+import { LocalStrategy } from './local.strategy';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule],
-  providers: [AuthService]
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET_KEY,
+      signOptions: { expiresIn: '300s' },
+    })
+  ],
+  providers: [AuthService, LocalStrategy],
+  controllers: [AuthController]
 })
 export class AuthModule {}
---
+++ src/auth/local.strategy.spec.ts
@@ -0,0 +1,47 @@
+import { UnauthorizedException } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from '../users/users.service';
+import { AuthService } from './auth.service';
+import { LocalStrategy } from './local.strategy';
+
+describe('LocalStrategy', () => {
+  let strategy: LocalStrategy;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [LocalStrategy, AuthService, UsersService],
+    }).compile();
+
+    strategy = module.get<LocalStrategy>(LocalStrategy);
+  });
+
+  it('should be defined', () => {
+    expect(strategy).toBeDefined();
+  });
+
+  describe('#validate', () => {
+    it('should return validated user if it exists', async () => {
+      const subject = await strategy.validate('user', 'pass');
+      expect(subject).toEqual({
+        userId: 1,
+        username: 'user'
+      });
+    });
+
+    it('should throw UnauthorizedException when username doesnt exist', async () => {
+        try {
+            const subject = await strategy.validate('not_user', 'pass');
+        } catch(e) {
+            expect(e).toEqual(new UnauthorizedException());
+        }
+    });
+
+    it('should throw UnauthorizedException when password is not correct', async () => {
+        try {
+            const subject = await strategy.validate('user', 'not_pass');
+        } catch(e) {
+            expect(e).toEqual(new UnauthorizedException());
+        }
+    });
+  });
+});
