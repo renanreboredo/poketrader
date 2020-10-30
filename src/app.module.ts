a32b2cfe12f8368f04856ba6af988ed866efa81f
Renan Reboredo <renanrreboredo@gmail.com>
2020-10-29 21:08:38 -0300

    Generate trade module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { PokeapiModule } from './pokeapi/pokeapi.module';
+import { TradeModule } from './trade/trade.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, PokeapiModule],
+  imports: [AuthModule, UsersModule, PokeapiModule, TradeModule],
   controllers: [AppController],
 })
 export class AppModule {}
